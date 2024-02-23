7e9a304197ae35ce8fcfda2953af07d3fc2ecae8
MK <m.kanamaru.it@gmail.com>
2024-02-22 23:25:14 -0800

    test toolit build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const App = () => {
-  return <h1>A React App</h1>;
+// const getItems = async () => {
+//   try {
+//     const response = await fetch("http://localhost:8080/api/items", {
+//       method: "GET",
+//       cache: "default",
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+const App = async () => {
+  // const items = await getItems();
+  // console.log(items);
+
+  return <h1>A React Apppp {items}</h1>;
 };
 
 export default App;
